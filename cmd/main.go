66e718d2890acd45f28fb3f5d21aeaba2b5f5cbd
MaximumTroubles <Rodziyevsky2@gmail.com>
2022-06-08 16:59:20 +0300

    installed migration on local machine, added migration files. added service file and repository file with interface and structs

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"log"
+
 	"github.com/MaximumTroubles/go-todo-app"
 	"github.com/MaximumTroubles/go-todo-app/pkg/handler"
-	"log"
+	"github.com/MaximumTroubles/go-todo-app/pkg/repository"
+	"github.com/MaximumTroubles/go-todo-app/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error intializing configs: %s", err.Error())
+	}
+
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
